d8d91551dcaa7edf8c3c5fa7c99b20a52fe7faa8
Tanmay <rajgortanmay95@gmail.com>
2021-10-16 14:44:20 -0400

    Initial commit

---
+++ app/src/main/java/com/tanmay/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tanmay.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view){
+        TextView text= findViewById(R.id.text);
+        text.setText("Hello Again");
+    }
+}
